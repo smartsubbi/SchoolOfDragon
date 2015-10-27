8be41a5541745139c5794a017ac3610f51b0535e
Subramanya <subbu.baliga@live.in>
2015-10-27 20:35:20 +0530

    New Test Case

--- com.SchoolOfDragon/src/test/java/UnitTest/ConfigDataProviderUnitTest.java
+++ com.SchoolOfDragon/src/test/java/UnitTest/ConfigDataProviderUnitTest.java
@@ -11,8 +11,12 @@ public class ConfigDataProviderUnitTest
 	@Test
 	public void testGetChromePath()
 	{
+//		String filePath = System.getProperty("user.dir") + "/SeleniumDrivers/chromedriver_win32/chromedriver.exe";
+//		System.out.println(filePath);
+//		System.out.println(System.getProperty("user.dir"));
 		ConfigDataProvider config = new ConfigDataProvider();
 		String chromePath = config.getChromePath();
+		
 		System.out.println("Unit Test Chromepath : "+chromePath);
 		Assert.assertEquals(chromePath, "./SeleniumDrivers/chromedriver_win32/chromedriver.exe");				
 	}
